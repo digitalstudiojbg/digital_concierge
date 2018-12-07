3663bb73548d5571ba0b763b38ca79eaeeb91179
jonathanadhitama <jonathanadhitama@users.noreply.github.com>
2018-12-07 16:29:40 +1100

    Create Category Page Enhancement
    Add Cancel Button
    Add Material UI Dialog to confirm user action for removing image and navigating away

--- cms/src/components/tablet/content/CreateCategory.js
+++ cms/src/components/tablet/content/CreateCategory.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ContainerDiv, CreateContentContainerDiv } from '../../../utils/Constants';
+import { ContainerDiv, CreateContentContainerDiv, TABLET_CMS_CREATE_CONTENT_INDEX_URL } from '../../../utils/Constants';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
@@ -9,11 +9,22 @@ import IconButton from '@material-ui/core/IconButton';
 import CancelIcon from "@material-ui/icons/Cancel";
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
 
 const styles = theme => ({
     saveButton: {
         color: "white",
         background: "#A5A4BF",
+        marginLeft: 0,
+    },
+    cancelButton: {
+        marginLeft: 30,
+        color: "#A2A2A2"
     },
     categoryNameTextField: {
         width: "100%",
@@ -28,15 +39,25 @@ const CategorySchema = Yup.object().shape({
     name: Yup.string().required('Required'),
 });
 
+function SlideUpTransition(props) {
+    return <Slide direction="up" {...props} />;
+}
+
 class CreateCategory extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
             imageName: "",
+            openDialog: false,
+            whichDialog: "",
         }
         this.changeImageName = this.changeImageName.bind(this);
         this.imageUploaderRef = React.createRef();
         this.removeImage = this.removeImage.bind(this);
+        this.navigateAway = this.navigateAway.bind(this);
+        this.openDialogImage = this.openDialogImage.bind(this);
+        this.openDialogCancel = this.openDialogCancel.bind(this);
+        this.closeDialog = this.closeDialog.bind(this);
     }
 
     changeImageName(imageName) {
@@ -45,6 +66,24 @@ class CreateCategory extends React.PureComponent {
 
     removeImage() {
         this.imageUploaderRef.removeImage();
+        this.setState({ openDialog: false, whichDialog: "" });
+    }
+
+    navigateAway() {
+        const { history } = this.props;
+        this.setState({ openDialog: false }, () => {
+            history.push(TABLET_CMS_CREATE_CONTENT_INDEX_URL);
+        });
+    }
+
+    openDialogImage() {
+        this.setState({ openDialog: true, whichDialog: "image" });
+    }
+    openDialogCancel() {
+        this.setState({ openDialog: true, whichDialog: "cancel" });
+    }
+    closeDialog() {
+        this.setState({ openDialog: false, whichDialog: "" });
     }
 
     render() {
@@ -61,11 +100,22 @@ class CreateCategory extends React.PureComponent {
                     }}
                     validationSchema={CategorySchema}
                 >
-                {({ isSubmitting, errors, values, touched }) => 
+                {({ isSubmitting, errors, values, }) => 
                     <Form>
                         <div style={{display: "flex", alignItems: "center", marginBottom: 40}}>
-                            <div style={{color: "rgb(35,38,92)", fontSize: "2.7em", width: "62%"}}>ADD CATEGORY</div>
-                            <div style={{color: "rgb(35,38,92)", fontSize: "1.2em", width: "10%"}}>
+                            <div style={{color: "rgb(35,38,92)", fontSize: "2.7em", width: "52%"}}>ADD CATEGORY</div>
+                            <div style={{width: "10%"}}>
+                                <Button 
+                                    className={classes.cancelButton}
+                                    disabled={ isSubmitting }
+                                    size="large"
+                                    variant="outlined"
+                                    onClick={this.openDialogCancel}
+                                >
+                                    CANCEL
+                                </Button>
+                            </div>
+                            <div style={{width: "10%"}}>
                                 <Button 
                                     type="submit" 
                                     disabled={ isSubmitting || Boolean(errors.name) || !Boolean(values.name) || values.name.length === 0 } 
@@ -120,7 +170,7 @@ class CreateCategory extends React.PureComponent {
                                         InputProps={{
                                             endAdornment:               
                                                 <InputAdornment position="end">
-                                                    <IconButton onClick={this.removeImage}>
+                                                    <IconButton disabled={this.state.imageName.length === 0} onClick={this.openImageDeleteConfirmation}>
                                                         <CancelIcon />
                                                     </IconButton>
                                                 </InputAdornment>,
@@ -132,6 +182,47 @@ class CreateCategory extends React.PureComponent {
                     </Form>
                 }
                 </Formik>
+                <Dialog
+                    open={this.state.openDialog}
+                    TransitionComponent={SlideUpTransition}
+                    keepMounted
+                    onClose={this.closeDialog}
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                    >
+                    <DialogTitle id="alert-dialog-slide-title">
+                        {this.state.whichDialog === "image" && <p>CONFIRM IMAGE DELETION</p>}
+                        {this.state.whichDialog === "cancel" && <p>CONFIRM PAGE NAVIGATION</p>}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-slide-description">
+                        {this.state.whichDialog === "image" && <p>PLEASE CONFIRM IF YOU WANT TO REMOVE CATEGORY IMAGE.</p>}
+                        {this.state.whichDialog === "cancel" && <p>ARE YOU SURE YOU WANT TO LEAVE THIS PAGE?</p>}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        {this.state.whichDialog === "image" && (
+                            <React.Fragment>
+                                <Button onClick={this.removeImage} color="primary">
+                                    YES
+                                </Button>
+                                <Button onClick={this.closeDialog} color="primary">
+                                    NO
+                                </Button>
+                            </React.Fragment>
+                        )}
+                        {this.state.whichDialog === "cancel" && (
+                            <React.Fragment>
+                                <Button onClick={this.navigateAway} color="primary">
+                                    LEAVE
+                                </Button>
+                                <Button onClick={this.closeDialog} color="primary">
+                                    STAY
+                                </Button>
+                            </React.Fragment>
+                        )}
+                    </DialogActions>
+                    </Dialog>
             </ContainerDiv>
         );
     }
