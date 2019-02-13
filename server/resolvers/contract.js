34e440b2def79e5fe7b8a380ceef2f5119bebe00
Neal Shen <shenyijie5580@gmail.com>
2019-02-13 14:29:21 +1100

    added mutation and file_key attribute for contract section

--- server/resolvers/contract.js
+++ server/resolvers/contract.js
@@ -1,4 +1,9 @@
 import db from "../models";
+import {
+    processUploadMedia,
+    processUpload,
+    processDelete
+} from "../utils/constant";
 
 export default {
     Query: {
@@ -6,6 +11,28 @@ export default {
         contracts: async () => await db.contract.findAll(),
         contractsByUser: async (_root, _input, { user }) =>
             await db.contract.findAll({ where: { clientId: user.clientId } })
+    },
+    Mutation: {
+        createContract: async (
+            _root,
+            { input: { number, agreement_date, renewal_date, clientId, file } },
+            { user, clientIp }
+        ) => {
+            let contract_file;
+            if (file) {
+                contract_file = await processUploadMedia(file, user.id, "file");
+            }
+
+            let created_contract = db.contract.build({
+                number,
+                agreement_date: new Date(agreement_date),
+                renewal_date: new Date(renewal_date),
+                file: "http://www.google.com.au",
+                clientId
+            });
+
+            return await created_contract.save();
+        }
     },
     Contract: {
         client: async contract => await db.client.findByPk(contract.clientId)
--- server/schemas/contract.js
+++ server/schemas/contract.js
@@ -7,15 +7,28 @@ export default gql`
         contractsByUser: [Contract]
     }
 
+    extend type Mutation {
+        createContract(input: CreateContractInput): Contract
+    }
+
     type Contract {
         id: ID!
         number: String
         file: URL
+        file_key: String
         agreement_date: DateTime
         renewal_date: DateTime
         active: Boolean
         createdAt: DateTime
         updatedAt: DateTime
         client: Client
+    }
+
+    input CreateContractInput {
+        number: String
+        file: Upload!
+        agreement_date: String
+        renewal_date: String
+        clientId: Int
     }
 `;
