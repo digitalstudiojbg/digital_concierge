a7ba31f787b2163c1419d709a4f2932f857bce07
jonathanadhitama <jonathanadhitama@users.noreply.github.com>
2019-02-28 10:46:04 +1100

    Client CMS
    -Open system in a new tab
    -Modify Sidebar to follow based on mockup
    -Add new url constants

--- clientcms/src/components/layout/Sidebar.js
+++ clientcms/src/components/layout/Sidebar.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Query, withApollo } from "react-apollo";
+import gql from "graphql-tag";
 import {
     WELCOME_URL,
     SYSTEM_CMS_INDEX_URL,
     SYSTEM_CMS_LANDINGPAGE_URL,
+    SYSTEM_CMS_HOME_URL,
     TOUCHSCREEN_CMS_INDEX_URL,
     SYSTEM_CMS_CONTENT_URL,
     SYSTEM_CMS_SETTINGS_URL,
     SYSTEM_CMS_CREATE_CONTENT_INDEX_URL,
     SYSTEM_CMS_CREATE_CONTENT_CATEGORY_URL,
     SYSTEM_CMS_CREATE_CONTENT_SUBCATEGORY_URL,
-    SYSTEM_CMS_CREATE_CONTENT_DIRECTORY_URL
+    SYSTEM_CMS_CREATE_CONTENT_DIRECTORY_URL,
+    SYSTEM_CMS_PROMOTION,
+    SYSTEM_CMS_LIBRARY,
+    SYSTEM_CMS_DEVICES,
+    SYSTEM_CMS_GUESTS
 } from "../../utils/Constants";
+import { withRouter } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import {
+    Visibility,
+    Home,
+    Dashboard,
+    Stars,
+    BookmarkBorder,
+    List,
+    Image,
+    LocalMovies,
+    TabletMac,
+    Settings,
+    People
+} from "@material-ui/icons";
+import loading from "../loading/Loading";
 
 const SIDEBAR_ITEMS = [
     {
@@ -19,44 +42,104 @@ const SIDEBAR_ITEMS = [
             process.env.NODE_ENV === "production"
                 ? "http://digitalconcierge-env.uir8vfstfw.ap-southeast-2.elasticbeanstalk.com/tablet/"
                 : "http://localhost:5000",
-        displayName: "View Site"
+        displayName: "View Site",
+        icon: Visibility
     },
-
+    {
+        name: SYSTEM_CMS_HOME_URL,
+        displayName: "Home",
+        icon: Home
+    },
     {
         name: SYSTEM_CMS_INDEX_URL,
-        displayName: "Dashboard"
+        displayName: "Dashboard",
+        icon: Dashboard
     },
     {
         name: SYSTEM_CMS_LANDINGPAGE_URL,
-        displayName: "Landing Page"
+        displayName: "System Start Page",
+        icon: Stars
+    },
+    {
+        name: SYSTEM_CMS_HOME_URL,
+        displayName: "System Home",
+        icon: BookmarkBorder
     },
     {
         name: SYSTEM_CMS_CONTENT_URL,
-        displayName: "Content"
+        displayName: "Content",
+        icon: List
     },
-
+    {
+        name: SYSTEM_CMS_PROMOTION,
+        displayName: "Promotions",
+        icon: Image
+    },
+    {
+        name: SYSTEM_CMS_LIBRARY,
+        displayName: "Library",
+        icon: LocalMovies
+    },
+    {
+        name: SYSTEM_CMS_DEVICES,
+        displayName: "Devices",
+        icon: TabletMac
+    },
     {
         name: SYSTEM_CMS_SETTINGS_URL,
-        displayName: "Settings"
+        displayName: "Settings",
+        icon: Settings
+    },
+    {
+        name: SYSTEM_CMS_GUESTS,
+        displayName: "Guests",
+        icon: People
     }
 ];
 
 const SidebarItem = styled.div`
-    height: 70px;
-    line-height: 70px;
+    /* height: 70px; */
+    /* line-height: 70px; */
+    flex: 1;
+    height: 100%;
     padding-left: 30px;
     transition: all 0.3s linear;
 
     background-color: ${({ selectedItem, expectedItem }) =>
-        selectedItem === expectedItem && "white"};
+        selectedItem === expectedItem && "rgb(43,43,43)"};
     color: ${({ selectedItem, expectedItem }) =>
-        selectedItem === expectedItem && "black"};
+        selectedItem === expectedItem && "rgb(234,234,234)"};
 
     &:hover {
         background-color: white;
         color: black;
     }
+
+    display: flex;
+    align-items: center;
 `;
+
+const SidebarLabel = styled.div`
+    font-size: 1.5em;
+    font-weight: 700;
+    padding-left: 10px;
+`;
+
+const ClientAvatarDiv = styled.div`
+    height: 115px;
+    background-image: url(${props => props.imageUrl});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-color: white;
+`;
+
+const styles = () => ({
+    icon: {
+        width: 30,
+        height: 30
+    }
+});
 
 class Sidebar extends Component {
     constructor(props) {
@@ -104,45 +187,114 @@ class Sidebar extends Component {
 
     render() {
         const { selectedItem } = this.state;
+        const { classes, match } = this.props;
+        const { params } = match || {};
+        const { system_id = "" } = params || {};
         return (
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    flexWrap: "nowrap",
-                    justifyContent: "flex-start",
-                    alignItems: "stretch",
-                    alignContent: "stretch",
-                    width: "350px",
-                    backgroundColor: "rgb(70,73,124)",
-                    color: "white",
-                    height: "calc(100vh-80px)"
-                }}
+            <Query
+                variables={{ id: system_id }}
+                query={gql`
+                    query getSystemDetailSidebar($id: ID!) {
+                        system(id: $id) {
+                            id
+                            name
+                            client {
+                                avatar
+                            }
+                        }
+                    }
+                `}
             >
-                {SIDEBAR_ITEMS.map((items, index) => {
-                    const { name, displayName, tablet } = items;
+                {({ loading, error, data: { system } }) => {
+                    if (loading) return <React.Fragment />;
+                    if (error) return <div />;
                     return (
-                        name &&
-                        displayName && (
-                            <SidebarItem
-                                key={index}
-                                onClick={() => {
-                                    this.setState({ selectedItem: name });
-                                    displayName === "View Site"
-                                        ? window.open(name, "_blank")
-                                        : name && this.props.history.push(name);
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                flexWrap: "nowrap",
+                                justifyContent: "flex-start",
+                                alignItems: "stretch",
+                                alignContent: "stretch",
+                                width: "350px",
+                                backgroundColor: "rgb(71,70,71)",
+                                color: "rgb(223,223,223)",
+                                height: "calc(100vh-80px)"
+                            }}
+                        >
+                            {Boolean(system) &&
+                                Boolean(system.client) &&
+                                Boolean(system.client.avatar) && (
+                                    <ClientAvatarDiv
+                                        imageUrl={system.client.avatar}
+                                    />
+                                )}
+                            <div
+                                style={{
+                                    padding: 10,
+                                    backgroundColor: "rgb(43,43,43)",
+                                    paddingLeft: 30
                                 }}
-                                selectedItem={selectedItem}
-                                expectedItem={name}
                             >
-                                <h1>{displayName}</h1>
-                            </SidebarItem>
-                        )
+                                <div
+                                    style={{
+                                        width: "50%",
+                                        height: "100%",
+                                        display: "flex",
+                                        alignItems: "center",
+                                        fontSize: "1.5em"
+                                    }}
+                                >
+                                    {system.name}
+                                </div>
+                            </div>
+                            {SIDEBAR_ITEMS.map((items, index) => {
+                                const {
+                                    name,
+                                    displayName,
+                                    icon: EntryIcon
+                                } = items;
+                                return (
+                                    name &&
+                                    displayName && (
+                                        <SidebarItem
+                                            key={index}
+                                            onClick={() => {
+                                                this.setState({
+                                                    selectedItem: name
+                                                });
+                                                displayName === "View Site"
+                                                    ? window.open(
+                                                          name,
+                                                          "_blank"
+                                                      )
+                                                    : name &&
+                                                      this.props.history.push(
+                                                          name
+                                                      );
+                                            }}
+                                            selectedItem={selectedItem}
+                                            expectedItem={name}
+                                        >
+                                            {Boolean(EntryIcon) && (
+                                                <EntryIcon
+                                                    className={classes.icon}
+                                                />
+                                            )}
+                                            <SidebarLabel>
+                                                {displayName}
+                                            </SidebarLabel>
+                                        </SidebarItem>
+                                    )
+                                );
+                            })}
+                        </div>
                     );
-                })}
-            </div>
+                }}
+            </Query>
         );
     }
 }
 
-export default Sidebar;
+export default withApollo(withRouter(withStyles(styles)(Sidebar)));
