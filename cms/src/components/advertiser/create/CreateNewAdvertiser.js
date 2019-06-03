6be7d9aaccdbb5e76c1e14438155b89bc5dc6b60
Jonathan Adhitama <jonathan.adhitama@gmail.com>
2019-06-03 16:26:24 +1000

    Prevent user from directly going to create advertiser page without going to Just Brilliant Guide List Page first

--- cms/src/components/advertiser/create/CreateNewAdvertiser.js
+++ cms/src/components/advertiser/create/CreateNewAdvertiser.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ModifyAdvertiser from "../common/ModifyAdvertiser";
 import { GUIDE_MAIN_URL } from "../../../utils/Constants";
+import { Redirect } from "react-router-dom";
 
-const CreateNewAdvertiser = ({
-    history: { push },
-    location: {
-        state: { pub_id }
-    }
-}) => (
-    <ModifyAdvertiser
-        has_data={false}
-        push={push}
-        pubId={pub_id}
-        exitUrl={GUIDE_MAIN_URL.replace(":pub_id", pub_id)}
-    />
+const CreateNewAdvertiser = ({ history: { push }, location }) => (
+    <React.Fragment>
+        {Boolean(location) &&
+        Boolean(location.state) &&
+        Boolean(location.state.pub_id) ? (
+            <ModifyAdvertiser
+                has_data={false}
+                push={push}
+                pubId={location.state.pub_id}
+                exitUrl={GUIDE_MAIN_URL.replace(
+                    ":pub_id",
+                    location.state.pub_id
+                )}
+            />
+        ) : (
+            <Redirect to="/main/guide" />
+        )}
+    </React.Fragment>
 );
 
 export default CreateNewAdvertiser;
