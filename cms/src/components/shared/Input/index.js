0f6fc730b411b82af15301c8bd02a122d4c55756
alexeimaltsev <alexq.dev@gmail.com>
2019-06-04 18:50:30 +0300

    Remove Kirill changes

--- cms/src/components/shared/Input/index.js
+++
@@ -1,7 +0,0 @@
-import Input from './Input';
-import Autocomplete from './Autocomplete';
-import Checkbox from './Checkbox';
-import DatePicker from './DatePicker';
-import TimePicker from './TimePicker';
-
-export { Input, Autocomplete, Checkbox, DatePicker, TimePicker };
