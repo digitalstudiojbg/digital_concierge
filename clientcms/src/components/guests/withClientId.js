b6410692ec8bc6a4007ed775530570ef21a8fe10
alexeimaltsev <alexq.dev@gmail.com>
2019-06-19 14:11:18 +0300

    Refactor withClientId

--- clientcms/src/components/guests/withClientId.js
+++ clientcms/src/components/guests/withClientId.js
@@ -3,16 +3,24 @@ import Query from "react-apollo/Query";
 import { withRouter } from "react-router-dom";
 import { getSystemDetail } from "../../data/query";
 
-const withClientId = Component => withRouter(React.memo((props) => {
-    const { system_id } = props.match.params;
+const withClientId = Component => withRouter(
+    React.memo((props) => {
+        const { system_id } = props.match.params;
 
-    return (
-        <Query query={getSystemDetail} variables={{ id: system_id }}>
-            {({ data: { system: data } }) => (
-                <Component {...props} clientId={data && data.client && Number(data.client.id)} />
-            )}
-        </Query>
-    )
-}));
+        return (
+            <Query
+                query={getSystemDetail}
+                variables={{ id: system_id }}
+            >
+                {({ data: { system: data } }) => (
+                    <Component
+                        {...props}
+                        clientId={data && data.client && Number(data.client.id)}
+                    />
+                )}
+            </Query>
+        )
+    })
+);
 
 export default withClientId;
