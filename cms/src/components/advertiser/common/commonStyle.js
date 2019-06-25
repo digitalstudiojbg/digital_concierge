89f9bc4bf80666ff25acd54b6cebaa30d9689dc3
Jaleh Pedram <jaleh@johnbatman.com.au>
2019-06-25 16:39:25 +1000

    Some changes on edit advertiser page

--- cms/src/components/advertiser/common/commonStyle.js
+++ cms/src/components/advertiser/common/commonStyle.js
@@ -31,3 +31,74 @@ export const ContinueButton = styled.button`
     background: #2699fb;
     border: 20px solid #313131;
 `;
+export const SectionHeader = styled.h4`
+    text-align: left;
+    color: #2699fb;
+    font-size: 20px;
+    padding: 0px;
+`;
+export const FieldLabel = styled.div`
+    font-size: 10px;
+    margin-bottom: 2px;
+    color: #5c5c5c;
+    text-transform: uppercase;
+`;
+export const FieldContainerDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    margin-bottom: 5px;
+`;
+
+export const MainSections = styled.div`
+    display: flex;
+    height: 100%;
+    width: 100%;
+`;
+
+export const SubSectionDiv = styled.div`
+    margin: 5px 0;
+
+    // display: flex;
+`;
+
+export const SectionContactTitleDiv = styled(SectionHeader)`
+    width: 100%;
+    display: flex;
+    align-items: center;
+`;
+
+export const FieldDivEqual = styled.div`
+    flex: 1;
+    padding-right: ${props => props.paddingRight};
+`;
+
+export const FieldDiv = styled.div`
+    flex-basis: ${props => props.flexBasis};
+    margin-right: ${props => props.marginRight};
+`;
+
+export const ContactEntryContainerDiv = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 10px;
+    padding-right: 10px;
+    padding-left: 10px;
+`;
+
+export const AddressContainerDiv = styled.div`
+    flex-basis: 100%;
+    margin-right: ${props => props.marginRight};
+    padding: 2%;
+    // border: 1px solid #707070;
+`;
+
+export const AddressSectionTitleDiv = styled.div`
+    color: #2699fb;
+    font-size: 1.4em;
+    font-weight: 600;
+    padding-bottom: 10px;
+`;
