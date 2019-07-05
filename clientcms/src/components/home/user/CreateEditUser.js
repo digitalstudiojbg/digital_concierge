a727c979666ae2096024f4a29d31a768c9d81e60
Jonathan Adhitama <jonathan.adhitama@gmail.com>
2019-07-05 17:26:30 +1000

    Add formik and section one

--- clientcms/src/components/home/user/CreateEditUser.js
+++ clientcms/src/components/home/user/CreateEditUser.js
@@ -3,6 +3,11 @@ import PropTypes from "prop-types";
 import CustomSaveButton from "../../../utils/CustomSaveButton";
 import { ContainerDiv } from "../../../utils/Constants";
 import styled from "styled-components";
+import { withStyles } from "@material-ui/core/styles";
+import { Formik, Form, Field } from "formik";
+import { TextField, Select } from "formik-material-ui";
+import { OutlinedInput, MenuItem } from "@material-ui/core";
+import { FieldContainerDiv, FieldLabel } from "./commonStyle";
 
 const ContainerDivModified = styled(ContainerDiv)`
     padding-left: 50px;
@@ -16,7 +21,141 @@ const FormContainerDiv = styled.div`
     flex-direction: row;
 `;
 
+const SectionDiv = styled.div`
+    width: ${props => props.width};
+    height: ${props => (Boolean(props.height) ? props.height : "100%")};
+    display: flex;
+    flex-direction: column;
+    padding: 0 4%;
+    border-right: ${props => (props.withBorder ? "1px solid #DDDDDD" : "none")};
+`;
+
+const SECTION_ONE_FIELDS = [
+    {
+        name: "name",
+        label: "USER",
+        required: true,
+        type: "text"
+    },
+    {
+        name: "email",
+        label: "EMAIL",
+        required: true,
+        type: "text"
+    }
+];
+const SECTION_TWO_FIELDS = ({ departmentList, roleList }) => [
+    {
+        name: "department",
+        label: "DEPARTMENT",
+        type: "select",
+        required: true,
+        optionList: departmentList
+    },
+    {
+        name: "role",
+        label: "ROLE",
+        type: "select",
+        required: true,
+        optionList: roleList
+    }
+];
+const SECTION_THREE_FIELDS = [
+    {
+        name: "password",
+        label: "PASSWORD",
+        required: false,
+        type: "password"
+    },
+    {
+        name: "confirm_password",
+        label: "CONFIRM PASSWORD",
+        required: false,
+        type: "password"
+    },
+    {
+        name: "active",
+        label: "STATUS",
+        type: "select",
+        required: true
+    }
+];
+
+const styles = () => ({
+    textInput: {
+        padding: "12px 10px",
+        backgroundColor: "white"
+    }
+});
+
 class CreateEditUser extends React.Component {
+    renderTextField = (name, label, required, type) => (
+        <FieldContainerDiv style={{ width: "100%" }}>
+            <FieldLabel>{label}</FieldLabel>
+            <Field
+                name={name}
+                required={required}
+                type={type}
+                component={TextField}
+                variant="outlined"
+                fullWidth={true}
+                inputProps={{
+                    style: {
+                        padding: "12px 10px",
+                        backgroundColor: "white"
+                    }
+                }}
+            />
+        </FieldContainerDiv>
+    );
+
+    renderSelectField = (nameValue, label, optionList) => {
+        // console.log(optionList);
+
+        return (
+            <React.Fragment>
+                <FieldLabel
+                    style={{
+                        color: "#5c5c5c",
+                        fontsize: "10px",
+                        marginbottom: "5px"
+                    }}
+                >
+                    {label}
+                </FieldLabel>
+                <Field
+                    style={{
+                        height: 43,
+                        backgroundColor: "white"
+                    }}
+                    name={nameValue}
+                    component={Select}
+                    disabled={optionList.length < 1}
+                    fullWidth={true}
+                    input={<OutlinedInput />}
+                >
+                    <MenuItem value="null" disabled>
+                        {label}
+                    </MenuItem>
+                    {optionList.map(({ id, name }, index) => (
+                        <MenuItem
+                            key={`ITEM-${name}-${id}-${index}`}
+                            value={id}
+                        >
+                            {name}
+                        </MenuItem>
+                    ))}
+                </Field>
+            </React.Fragment>
+        );
+    };
+
+    renderField(fieldData) {
+        const { type, name, label, required } = fieldData;
+        if (type === "text" || type === "password") {
+            return this.renderTextField(name, label, required, type);
+        }
+    }
     renderHeaderSection() {
         const { match = {} } = this.props;
         const { params = {} } = match;
@@ -49,13 +188,40 @@ class CreateEditUser extends React.Component {
         );
     }
     renderFormSection() {
-        return <FormContainerDiv>TEST</FormContainerDiv>;
+        return (
+            <FormContainerDiv>
+                <SectionDiv width="33%" withBorder>
+                    {this.renderSectionOne()}
+                </SectionDiv>
+                <SectionDiv width="33%" withBorder>
+                    SECTION TWO
+                </SectionDiv>
+                <SectionDiv width="33%">SECTION THREE</SectionDiv>
+            </FormContainerDiv>
+        );
+    }
+    renderSectionOne() {
+        return (
+            <React.Fragment>
+                {SECTION_ONE_FIELDS.map((fieldData, index) => (
+                    <React.Fragment key={`SECTION-ONE-${index}`}>
+                        {this.renderField(fieldData)}
+                    </React.Fragment>
+                ))}
+            </React.Fragment>
+        );
     }
     render() {
         return (
             <ContainerDivModified>
-                {this.renderHeaderSection()}
-                {this.renderFormSection()}
+                <Formik>
+                    {() => (
+                        <Form>
+                            {this.renderHeaderSection()}
+                            {this.renderFormSection()}
+                        </Form>
+                    )}
+                </Formik>
             </ContainerDivModified>
         );
     }
@@ -70,4 +236,4 @@ CreateEditUser.propTypes = {
     ).isRequired
 };
 
-export default CreateEditUser;
+export default withStyles(styles)(CreateEditUser);
---
+++ clientcms/src/components/home/user/commonStyle.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const FieldLabel = styled.div`
+    font-size: 10px;
+    margin-bottom: 2px;
+    color: #5c5c5c;
+    text-transform: uppercase;
+`;
+export const FieldContainerDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    margin-bottom: 5px;
+`;
